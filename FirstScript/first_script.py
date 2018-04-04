2f4110cfce1eba80f9a3f4f1ad4e5b46b4b063a9
Sohesh Doshi <sohesh@promact.com>
2018-04-04 17:22:28 +0530

    Add Logger Fun

--- FirstScript/first_script.py
+++ FirstScript/first_script.py
@@ -2,7 +2,7 @@ import hashlib
 import os
 import sys
 import json
-import time
+import logging
 
 def md5sum(filename):
     """
@@ -15,10 +15,15 @@ def md5sum(filename):
     -------
         md5.hexdigest() is returned string containing only hexadecimal digits.
     """
+    logging.info('Enter into md5sum.')
     md5 = hashlib.md5()
+    logging.info('MD5 Object Create.')
     with open(filename, 'rb') as f:
+        logging.info('Open File %s',filename)
         for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
             md5.update(chunk)
+        logging.info('Generating Hash.')
+        logging.info('Exit Md5.')
     return md5.hexdigest()
 
 #FUNCTION FOR WRITE DATA INTO JSONFile By Default Path is Current Path & Filename:- last_data_json.json.
@@ -40,9 +45,12 @@ def writeToJSONFile(path, filename, data):
         No Returns Only write JsonData in file
 
     """
+    logging.info('Enter into writeToJson.')
     filePathNameExt='./'+path+'/'+filename
+    logging.info('filePathNameExt %s',filePathNameExt)
     with open(filePathNameExt,'w') as fp:
         json.dump(data, fp)
+    logging.info('writeToJson Exit.')
 
 
 #Function For Check Json File is Exists or Not if File is Not Present It Create Automatically.
@@ -63,106 +71,78 @@ def checkJsonFile(path, filename):
         Only Checks FileExists If Not then Create It.
 
     """
+    logging.info('Enter into checkJsonFile.')
     filePathNameExt='./'+path+'/'+filename
     if os.path.isfile(filePathNameExt):
+        logging.info('Pass.')
         pass
     else:
         with open(filePathNameExt,'w') as fp:
+            logging.info('%s File Created',filePathNameExt)
             fp.close()
+    logging.info('checkJson Complete.')
+
+def file_Changes(argv_one):
+    """
+    Returns
+    -------
+    data:dict
+        if any changes in file function return that data in dict
+    Examples:
+        {'created': [C:\\Users\\xyz.txt], 'deleted': [C:\\Users\\abc.txt], 'rename': [C:\\Users\\ex.txt]}
+    """
+    logging.info('Enter Into file_Changes.')
+    logging.info('%s [folder path]:-',argv_one)
+    fileDict = {}
+    rootdir = argv_one
+    path='./'
+    fileName='last_data_json'
+    suffix='.json'
+    data={}
+    checkJsonFile(path,fileName+suffix)
+    if os.stat(os.path.join(path,fileName+suffix)).st_size == 0:
+        logging.info('Filesize 0 Found.')
+        for root, subFolders, files in os.walk(rootdir):
+            for file in files:
+                fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
+        writeToJSONFile(path, fileName+suffix, fileDict)
+    else:
+        file_Change_list=[]
+        logging.info('file_Change_List Generate.')
+        with open(os.path.join(path,fileName+suffix)) as json_data:
+            last_json_dict = json.load(json_data)
+            logging.info('last_json_dict created.')
+        for root, subFolders, files in os.walk(rootdir):
+            for file in files:
+                fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
+        data['created']=list(set(fileDict)-set(last_json_dict))
+        logging.info('File Created Found.')
+        data['deleted']=list(set(last_json_dict)-set(fileDict))
+        logging.info('File Deleted Found.')
+        inv_map_fileDict = {v: k for k, v in fileDict.items()}
+        logging.info('Inverse fileDict.')
+        inv_map_json = {v: k for k, v in last_json_dict.items()}
+        logging.info('Inverse JsonDict.')
+        #result = set(inv_map_fileDict) - set(inv_map_json)
+        for i in set(inv_map_fileDict) - set(inv_map_json):
+            file_Change_list+=inv_map_fileDict[i]
+        data['file_changes'] = file_Change_list
+        logging.info('File_changes Detected.')
+        writeToJSONFile(path,fileName+suffix, fileDict)
+        return json.dumps(data)
 
 
-#If command line argument is Not 2 raise error
+
 
 if len(sys.argv) == 2:
-    def file_Changes():
-        """
-        Returns
-        -------
-        data:dict
-            if any changes in file function return that data in dict
-        Examples:
-            {'created': [], 'deleted': [], 'rename': []}
-        """
-        fileDict = {}
-        rootdir = sys.argv[1]
-        path='./'
-        fileName='last_data_json'
-        suffix='.json'
-        data={}
-        checkJsonFile(path,fileName+suffix)
-        if os.stat(os.path.join(path,fileName+suffix)).st_size == 0:
-            for root, subFolders, files in os.walk(rootdir):
-                for file in files:
-                    fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
-            writeToJSONFile(path, fileName+suffix, fileDict)
-        else:
-            file_Change_list=[]
-            with open(os.path.join(path,fileName+suffix)) as json_data:
-                last_json_dict = json.load(json_data)
-            for root, subFolders, files in os.walk(rootdir):
-                for file in files:
-                    fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
-            data['created']=list(set(fileDict)-set(last_json_dict))
-            data['deleted']=list(set(last_json_dict)-set(fileDict))
-            inv_map_fileDict = {v: k for k, v in fileDict.items()}
-            inv_map_json = {v: k for k, v in last_json_dict.items()}
-            #result = set(inv_map_fileDict) - set(inv_map_json)
-            for i in set(inv_map_fileDict) - set(inv_map_json):
-                file_Change_list+=inv_map_fileDict[i]
-            data['file_changes'] = file_Change_list
-            writeToJSONFile(path,fileName+suffix, fileDict)
-            return json.dumps(data)
-
-elif len(sys.argv) == 3 and sys.argv[2] == "-d":
-    def file_Changes():
-        """
-        Returns
-        -------
-        data:dict
-            if any changes in file function return that data in dict
-        Examples:
-            {'created': [], 'deleted': [], 'rename': []}
-        """
-        fileDict = {}
-        rootdir = sys.argv[1]
-        path='./'
-        fileName='last_data_json'
-        suffix='.json'
-        data={}
-        print("Enter Inside the Function file_Changes()")
-        checkJsonFile(path,fileName+suffix)
-        print("checkJsonFile_Function Complete")
-        if os.stat(os.path.join(path,fileName+suffix)).st_size == 0:
-            print("Size 0 Found")
-            for root, subFolders, files in os.walk(rootdir):
-                for file in files:
-                    fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
-            writeToJSONFile(path, fileName+suffix, fileDict)
-            print("WriteToJSONFile Complete")
-        else:
-            file_Change_list=[]
-            with open(os.path.join(path,fileName+suffix)) as json_data:
-                print("Open File For Load Json Data")
-                last_json_dict = json.load(json_data)
-                print("JsonLoad Complete")
-            for root, subFolders, files in os.walk(rootdir):
-                for file in files:
-                    fileDict[os.path.join(root, file)] = md5sum(os.path.join(root, file))
-            data['created']=list(set(fileDict)-set(last_json_dict))
-            print("created file check")
-            data['deleted']=list(set(last_json_dict)-set(fileDict))
-            print("deleted file check")
-            inv_map_fileDict = {v: k for k, v in fileDict.items()}
-            print("invert Dict")
-            inv_map_json = {v: k for k, v in last_json_dict.items()}
-            print("invert JsonDict")
-            for i in set(inv_map_fileDict) - set(inv_map_json):
-                file_Change_list+=inv_map_fileDict[i]
-            data['file_changes'] = file_Change_list
-            print("file changes check")
-            writeToJSONFile(path,fileName+suffix, fileDict)
-            print("WriteToJSONFile Complete")
-            return json.dumps(data)
+    argv_one=sys.argv[1]
+    file_Changes(argv_one)
+elif len(sys.argv) == 3 and sys.argv[2]=='-d':
+    argv_one=sys.argv[1]
+    logging.basicConfig(filename='exmple.log',filemode='w',level=logging.DEBUG,format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M')
+    logging.info('Start')
+    file_Changes(argv_one)
+    logging.info('End')
 else:
     print('''USAGE
     'Ex. python script.py [Folder_Name]' or
